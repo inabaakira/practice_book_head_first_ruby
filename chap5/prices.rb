c60b9843bc25355a4827707f032e82a16eb04454
INABA Akira <inaba@ulula.local>
2017-09-08 00:23:00 +0900

    [Chap5] The end of the section: Eliminating repetition...the WRONG way...

--- chap5/prices.rb
+++ chap5/prices.rb
@@ -2,39 +2,33 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: prices.rb
 #    Created:       <2017/09/07 22:27:23>
-#    Last Modified: <2017/09/07 22:44:41>
+#    Last Modified: <2017/09/08 00:21:32>
 
-def total(prices)
-  amount = 0
+def do_something_with_every_item(array, operation)
+  if operation == "total" or operation == "refund"
+    amount = 0
+  end
   index = 0
-  while index < prices.length
-    amount += prices[index]
+  while index < array.length
+    if operation == "total"
+      amount += array[index]
+    elsif operation == "refund"
+      amount -= array[index]
+    elsif operation == "show discounts"
+      amount_off = array[index] / 3.0
+      puts format("Your discount: $%.2f", amount_off)
+    end
+
     index += 1
   end
-  amount
-end
 
-def refund(prices)
-  amount = 0
-  index = 0
-  while index < prices.length
-    amount -= prices[index]
-    index += 1
-  end
-  amount
-end
-
-def show_discount(prices)
-  index = 0
-  while index < prices.length
-    amount_off = prices[index] / 3.0
-    puts format("Your discount: $%.2f", amount_off)
-    index += 1
+  if operation == "total" or operation == "refund"
+    return amount
   end
 end
 
 prices = [3.99, 25.00, 8.99]
 
-puts format("%.2f", total(prices))
-puts format("%.2f", refund(prices))
-show_discount(prices)
+puts format("%.2f", do_something_with_every_item(prices, "total"))
+puts format("%.2f", do_something_with_every_item(prices, "refund"))
+do_something_with_every_item(prices, "show discounts")
