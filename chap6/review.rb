1830d1e74ace13e8a32a20b170788fc025275e16
INABA Akira <inaba@ulula.local>
2017-10-07 12:51:10 +0900

    [chap6] The end of the section: Safely closing the file

--- chap6/review.rb
+++ chap6/review.rb
@@ -2,10 +2,11 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: review.rb
 #    Created:       <2017/10/07 12:46:23>
-#    Last Modified: <2017/10/07 12:48:45>
+#    Last Modified: <2017/10/07 12:51:02>
 
 review_file = File.open("reviews.txt")
 puts review_file.class
 lines = review_file.readlines
+review_file.close
 puts "Line 4: #{lines[3]}"
 puts "Line 1: #{lines[0]}"
