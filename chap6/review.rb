a9f6c6ce06de31df627c4755587a640b2d0b0922
INABA Akira <inaba@ulula.local>
2017-10-07 13:50:07 +0900

    [chap6] The end of the section: Don't forget about variable scope!

--- chap6/review.rb
+++ chap6/review.rb
@@ -2,10 +2,13 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: review.rb
 #    Created:       <2017/10/07 12:46:23>
-#    Last Modified: <2017/10/07 12:55:35>
+#    Last Modified: <2017/10/07 13:01:20>
+
+lines = []
 
 File.open("reviews.txt") do |review_file|
   lines = review_file.readlines
 end
+puts lines.length
 puts "Line 4: #{lines[3]}"
 puts "Line 1: #{lines[0]}"
