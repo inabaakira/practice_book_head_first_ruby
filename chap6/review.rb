42bc1179bf79cb82be3c937e1e659a43377bd4d5
INABA Akira <inaba@ulula.local>
2017-10-08 01:36:28 +0900

    [chap6] The end of the section: The verbose way to find array elements, using "each"

--- chap6/review.rb
+++ chap6/review.rb
@@ -2,13 +2,20 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: review.rb
 #    Created:       <2017/10/07 12:46:23>
-#    Last Modified: <2017/10/07 13:01:20>
+#    Last Modified: <2017/10/07 20:55:26>
 
 lines = []
 
 File.open("reviews.txt") do |review_file|
   lines = review_file.readlines
 end
-puts lines.length
-puts "Line 4: #{lines[3]}"
-puts "Line 1: #{lines[0]}"
+
+relevant_lines = []
+
+lines.each do |line|
+  if line.include?("Truncated")
+    relevant_lines << line
+  end
+end
+
+puts relevant_lines
