01cc117d54dafe774973c955cd9659c046abea80
INABA Akira <inaba@ulula.local>
2017-10-08 02:19:12 +0900

    [chap6] The end of the section: Introducing a faster method...

--- chap6/review.rb
+++ chap6/review.rb
@@ -2,7 +2,7 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: review.rb
 #    Created:       <2017/10/07 12:46:23>
-#    Last Modified: <2017/10/07 20:55:26>
+#    Last Modified: <2017/10/08 01:46:38>
 
 lines = []
 
@@ -10,12 +10,6 @@ File.open("reviews.txt") do |review_file|
   lines = review_file.readlines
 end
 
-relevant_lines = []
-
-lines.each do |line|
-  if line.include?("Truncated")
-    relevant_lines << line
-  end
-end
+relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 
 puts relevant_lines
