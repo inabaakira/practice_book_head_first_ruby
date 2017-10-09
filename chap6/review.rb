12aa605787e2e3842f91b8e3fff328d6d9425ad2
INABA Akira <inaba@ulula.local>
2017-10-09 13:06:54 +0900

    [Chap6] The end of the section: Finding the index of an array element

--- chap6/review.rb
+++ chap6/review.rb
@@ -2,7 +2,13 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: review.rb
 #    Created:       <2017/10/07 12:46:23>
-#    Last Modified: <2017/10/08 01:46:38>
+#    Last Modified: <2017/10/09 13:06:05>
+
+def find_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
 
 lines = []
 
@@ -11,5 +17,11 @@ File.open("reviews.txt") do |review_file|
 end
 
 relevant_lines = lines.find_all { |line| line.include?("Truncated") }
+reviews = relevant_lines.reject { |line| line.include?("--") }
 
-puts relevant_lines
+adjectives = []
+reviews.each do |review|
+  adjectives << find_adjective(review)
+end
+
+puts adjectives
