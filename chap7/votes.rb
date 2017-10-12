cf94e67ed4dacc99a85aede29096879f6542b378
INABA Akira <inaba@ulula.local>
2017-10-13 00:08:34 +0900

    [Chap.7] The end of the section: nil (and only nil) is "falsy"

--- chap7/votes.rb
+++ chap7/votes.rb
@@ -2,7 +2,7 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: votes.rb
 #    Created:       <2017/10/12 20:52:31>
-#    Last Modified: <2017/10/12 23:48:39>
+#    Last Modified: <2017/10/12 23:56:30>
 
 lines = []
 File.open("votes.txt") do |file|
@@ -12,7 +12,7 @@ end
 votes = {}
 lines.each do |line|
   name = line.chomp
-  if votes[name] != nil
+  if votes[name]
     votes[name] += 1
   else
     votes[name] = 1
