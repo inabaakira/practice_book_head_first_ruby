49e136ab65e35656a9c1ba4750180c713716d13e
INABA Akira <inaba@ulula.local>
2017-10-12 23:49:04 +0900

    [Chap.7] The end of the section: Hashes return "nil" by default

--- chap7/votes.rb
+++ chap7/votes.rb
@@ -2,9 +2,21 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: votes.rb
 #    Created:       <2017/10/12 20:52:31>
-#    Last Modified: <2017/10/12 20:53:05>
+#    Last Modified: <2017/10/12 23:48:39>
 
 lines = []
 File.open("votes.txt") do |file|
   lines = file.readlines
 end
+
+votes = {}
+lines.each do |line|
+  name = line.chomp
+  if votes[name] != nil
+    votes[name] += 1
+  else
+    votes[name] = 1
+  end
+end
+
+p votes
