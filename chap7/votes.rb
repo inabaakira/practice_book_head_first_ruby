367ff3f2defc5d9469e2fdeca3667000f952bb76
INABA Akira <inaba@ulula.local>
2017-10-14 18:43:37 +0900

    [Chap.7] The end of the section: Normalizing hash keys

--- chap7/votes.rb
+++ chap7/votes.rb
@@ -2,7 +2,7 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: votes.rb
 #    Created:       <2017/10/12 20:52:31>
-#    Last Modified: <2017/10/14 13:51:58>
+#    Last Modified: <2017/10/14 18:40:51>
 
 lines = []
 File.open("votes.txt") do |file|
@@ -12,6 +12,7 @@ end
 votes = Hash.new(0)
 lines.each do |line|
   name = line.chomp
+  name.upcase!
   votes[name] += 1
 end
 
