9e0261d1dfea1cb9f9c68393a11e517366e3661e
INABA Akira <inaba@ulula.local>
2017-10-16 17:09:52 +0900

    [Chap.8] The end of the section: Quickly identifying objects with "inspect"

--- chap8/celestial_body.rb
+++ chap8/celestial_body.rb
@@ -2,7 +2,7 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: celestial_body.rb
 #    Created:       <2017/10/16 12:11:10>
-#    Last Modified: <2017/10/16 16:23:09>
+#    Last Modified: <2017/10/16 17:07:23>
 
 class CelestialBody
   attr_accessor :type, :name
@@ -11,15 +11,12 @@ end
 altair = CelestialBody.new
 altair.name = 'Altair'
 altair.type = 'star'
-polaris = CelestialBody.new
+polaris = altair
 polaris.name = 'Polaris'
-polaris.type = 'star'
+
+# puts altair.object_id, polaris.object_id
+
+puts altair.inspect, polaris.inspect
+
 vega = CelestialBody.new
-vega.name = 'Vega'
-vega.type = 'star'
-
-puts altair.name, polaris.name, vega.name
-
-puts altair.object_id
-puts polaris.object_id
-puts vega.object_id
+puts vega.inspect
