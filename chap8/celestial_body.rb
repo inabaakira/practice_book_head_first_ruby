64e65ceabd92e681c9e24c29af14d0beb82ec66c
INABA Akira <inaba@ulula.local>
2017-10-16 19:14:04 +0900

    [Chap.8] The end of the section: Problems with a hash default object

--- chap8/celestial_body.rb
+++ chap8/celestial_body.rb
@@ -2,21 +2,25 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: celestial_body.rb
 #    Created:       <2017/10/16 12:11:10>
-#    Last Modified: <2017/10/16 17:07:23>
+#    Last Modified: <2017/10/16 19:03:55>
 
 class CelestialBody
   attr_accessor :type, :name
 end
 
-altair = CelestialBody.new
-altair.name = 'Altair'
-altair.type = 'star'
-polaris = altair
-polaris.name = 'Polaris'
+default_body = CelestialBody.new
+default_body.type = 'planet'
+bodies = Hash.new(default_body)
 
-# puts altair.object_id, polaris.object_id
+bodies['Mars'].name = 'Mars'
+p bodies['Mars']
 
-puts altair.inspect, polaris.inspect
+bodies['Europa'].name = 'Europa'
+bodies['Europa'].type = 'moon'
+p bodies['Europa']
 
-vega = CelestialBody.new
-puts vega.inspect
+bodies['Venus'].name = 'Venus'
+p bodies['Venus']
+
+p bodies['Mars']
+p bodies['Europa']
