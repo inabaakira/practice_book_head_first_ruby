8f55fa3b8a7235d5dcd52039d0f1c797fcf40d20
INABA Akira <inaba@ulula.local>
2017-10-16 16:24:31 +0900

    [Chap.8] The end of the section: Fixing the astronomer's program

--- chap8/celestial_body.rb
+++ chap8/celestial_body.rb
@@ -2,7 +2,7 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: celestial_body.rb
 #    Created:       <2017/10/16 12:11:10>
-#    Last Modified: <2017/10/16 12:13:17>
+#    Last Modified: <2017/10/16 16:23:09>
 
 class CelestialBody
   attr_accessor :type, :name
@@ -11,9 +11,15 @@ end
 altair = CelestialBody.new
 altair.name = 'Altair'
 altair.type = 'star'
-polaris = altair
+polaris = CelestialBody.new
 polaris.name = 'Polaris'
-vega = polaris
+polaris.type = 'star'
+vega = CelestialBody.new
 vega.name = 'Vega'
+vega.type = 'star'
 
 puts altair.name, polaris.name, vega.name
+
+puts altair.object_id
+puts polaris.object_id
+puts vega.object_id
