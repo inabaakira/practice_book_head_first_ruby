7a6f5ec1eb0d9071338b0f9568ab887fdece003d
INABA Akira <inaba@ulula.local>
2017-10-20 00:19:03 +0900

    [Chap.8] The end of the section: Back to the hash of planets and moons

--- chap8/celestial_body.rb
+++ chap8/celestial_body.rb
@@ -2,7 +2,7 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: celestial_body.rb
 #    Created:       <2017/10/16 12:11:10>
-#    Last Modified: <2017/10/16 19:03:55>
+#    Last Modified: <2017/10/20 00:18:59>
 
 class CelestialBody
   attr_accessor :type, :name
@@ -13,14 +13,8 @@ default_body.type = 'planet'
 bodies = Hash.new(default_body)
 
 bodies['Mars'].name = 'Mars'
-p bodies['Mars']
-
 bodies['Europa'].name = 'Europa'
 bodies['Europa'].type = 'moon'
-p bodies['Europa']
-
 bodies['Venus'].name = 'Venus'
-p bodies['Venus']
 
-p bodies['Mars']
-p bodies['Europa']
+p bodies
