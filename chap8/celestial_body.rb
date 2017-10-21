58818e28aa8fee84480e6adf41e9858a92cd8f8a
INABA Akira <inaba@ulula.local>
2017-10-21 15:18:57 +0900

    [Chap.8] The end of the section: Hash default blocks

--- chap8/celestial_body.rb
+++ chap8/celestial_body.rb
@@ -2,19 +2,26 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: celestial_body.rb
 #    Created:       <2017/10/16 12:11:10>
-#    Last Modified: <2017/10/20 00:18:59>
+#    Last Modified: <2017/10/21 15:18:31>
 
 class CelestialBody
   attr_accessor :type, :name
 end
 
-default_body = CelestialBody.new
-default_body.type = 'planet'
-bodies = Hash.new(default_body)
+bodies = Hash.new do |hash, key|
+  body = CelestialBody.new
+  body.type = "planet"
+  hash[key] = body
+  body
+end
 
 bodies['Mars'].name = 'Mars'
 bodies['Europa'].name = 'Europa'
 bodies['Europa'].type = 'moon'
 bodies['Venus'].name = 'Venus'
+
+p bodies['Mars']
+p bodies['Europa']
+p bodies['Venus']
 
 p bodies
