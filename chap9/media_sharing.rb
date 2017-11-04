422677cad63e4a8ae42c91d294f52348d1873278
INABA Akira <inaba@ulula.local>
2017-11-04 20:05:05 +0900

    [Chap.9] The end of the section: Creating a mixin for comments

--- chap9/media_sharing.rb
+++ chap9/media_sharing.rb
@@ -2,36 +2,24 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: media_sharing.rb
 #    Created:       <2017/10/29 13:49:49>
-#    Last Modified: <2017/10/29 13:55:05>
+#    Last Modified: <2017/11/04 20:04:44>
 
-class Clip
-  attr_reader :comments
-
-  def initialize
-    @comments = []
+module AcceptsComments
+  def commants
+    if @comments
+      @comments
+    else
+      @comments = []
+    end
   end
 
   def add_comment(comment)
     comments << comment
   end
+end
 
+class Clip
   def play
     puts "Playing #{object_id}..."
   end
 end
-
-class Video < Clip
-  attr_accessor :resolution
-end
-
-class Song < Clip
-  attr_accessor :beats_per_minute
-end
-
-video = Video.new
-video.add_comment("Cool slow motion effect!")
-video.add_comment("Weired ending.")
-song = Song.new
-song.add_comment("Awesome beat.")
-
-p video.comments, song.comments
