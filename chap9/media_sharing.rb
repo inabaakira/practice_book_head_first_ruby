a19eda3804bf8f643f15e393e293be9e8897ed8c
INABA Akira <inaba@ulula.local>
2017-11-05 09:47:34 +0900

    [Chap.9] The end of the section: Using our comments mixin

--- chap9/media_sharing.rb
+++ chap9/media_sharing.rb
@@ -2,10 +2,10 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: media_sharing.rb
 #    Created:       <2017/10/29 13:49:49>
-#    Last Modified: <2017/11/04 20:04:44>
+#    Last Modified: <2017/11/05 09:47:24>
 
 module AcceptsComments
-  def commants
+  def comments
     if @comments
       @comments
     else
@@ -23,3 +23,38 @@ class Clip
     puts "Playing #{object_id}..."
   end
 end
+
+class Video < Clip
+  include AcceptsComments
+  attr_accessor :resolution
+end
+
+class Song < Clip
+  include AcceptsComments
+  attr_accessor :beats_per_minute
+end
+
+class Photo
+  include AcceptsComments
+  def show
+    puts "Displaying #{object_id}..."
+  end
+end
+
+video = Video.new
+video.add_comment("Cool slow motion effect!")
+video.add_comment("Weired ending.")
+song = Song.new
+song.add_comment("Awesome beat.")
+
+p video.comments, song.comments
+
+video.play
+song.play
+
+photo = Photo.new
+photo.add_comment("Beautiful colors.")
+
+p photo.comments
+
+photo.show
