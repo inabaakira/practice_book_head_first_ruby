13bcce0a6ad0ba7a6c6ded1ce8478c78dcd4bcce
INABA Akira <inaba@ulula.local>
2017-11-14 22:16:08 +0900

    [Chap.10] The end of the section: Implementing a greater-than method on the Steak class

---
+++ chap10/steak.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+#-*- mode: ruby; coding: utf-8 -*-
+# file: steak.rb
+#    Created:       <2017/11/14 21:51:14>
+#    Last Modified: <2017/11/14 21:55:10>
+
+class Steak
+  attr_accessor :grade
+
+  def >(other)
+    grade_scores = {"Prime" => 3, "Choice" => 2, "Select" => 1}
+    grade_scores[grade] > grade_scores[other.grade]
+  end
+end
+
+first_steak = Steak.new
+first_steak.grade = "Prime"
+second_steak = Steak.new
+second_steak.grade = "Choice"
+
+if first_steak > second_steak
+  puts "I'll take #{first_steak.inspect}"
+end
