89d264d5c103cdffc63edf9832c506a73f2a9656
INABA Akira <inaba@ulula.local>
2017-11-14 23:32:04 +0900

    [Chap.10] The end of the section: Implementing the spaceship operator on Steak

--- chap10/steak.rb
+++ chap10/steak.rb
@@ -2,15 +2,21 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: steak.rb
 #    Created:       <2017/11/14 21:51:14>
-#    Last Modified: <2017/11/14 22:19:52>
+#    Last Modified: <2017/11/14 23:02:26>
 
 class Steak
   GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
 
   attr_accessor :grade
 
-  def >(other)
-    GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
+  def <=>(other)
+    if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
+      return -1
+    elsif GRADE_SCORES[self.grade] == GRADE_SCORES[other.grade]
+      return 0
+    else
+      return 1
+    end
   end
 end
 
@@ -19,6 +25,5 @@ first_steak.grade = "Prime"
 second_steak = Steak.new
 second_steak.grade = "Choice"
 
-if first_steak > second_steak
-  puts "I'll take #{first_steak.inspect}"
-end
+puts first_steak <=> second_steak
+puts second_steak <=> first_steak
