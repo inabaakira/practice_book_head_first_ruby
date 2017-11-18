458e7b36a1c1d1319b91542daed665c70aa099ee
INABA Akira <inaba@ulula.local>
2017-11-18 20:27:29 +0900

    [Chap.10] The end of the section: Inside the Enumerable module

--- chap10/word_splitter.rb
+++ chap10/word_splitter.rb
@@ -2,9 +2,10 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: word_splitter.rb
 #    Created:       <2017/11/15 20:28:47>
-#    Last Modified: <2017/11/15 20:52:45>
+#    Last Modified: <2017/11/18 20:17:31>
 
 class WordSplitter
+
   include Enumerable
 
   attr_accessor :string
@@ -15,15 +16,3 @@ class WordSplitter
     end
   end
 end
-
-splitter = WordSplitter.new
-splitter.string = "how do you do"
-
-p splitter.find_all { |word| word.include?("d") }
-p splitter.reject { |word| word.include?("d") }
-p splitter.map { |word| word.reverse }
-
-p splitter.any? { |word| word.include?("e") }
-p splitter.count
-p splitter.first
-p splitter.sort
