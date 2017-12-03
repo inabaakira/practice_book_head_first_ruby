a84857ca2fccf34f87f0f775bc669d40f1447c89
INABA Akira <inaba@ulula.local>
2017-12-04 00:05:28 +0900

    [Chap.12] The end of the section: Multiple rescue clauses in one begin/end block

--- chap12/eat.rb
+++ chap12/eat.rb
@@ -2,7 +2,7 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: eat.rb
 #    Created:       <2017/12/03 22:24:53>
-#    Last Modified: <2017/12/03 23:55:19>
+#    Last Modified: <2017/12/04 00:03:55>
 
 class PorridgeError < StandardError
 end
@@ -18,14 +18,18 @@ def sleep
   raise BeddingError, "too soft"
 end
 
-# begin
-#   eat
-# rescue BeddingError => error
-#   puts "This bed is #{error.message}!"
-# end
+begin
+  eat
+rescue BeddingError => error
+  puts "This bed is #{error.message}!"
+rescue PorridgeError => error
+  puts "This porridge is #{error.message}!"
+end
 
 begin
   sleep
 rescue BeddingError => error
   puts "This bed is #{error.message}!"
+rescue PorridgeError => error
+  puts "This porridge is #{error.message}!"
 end
