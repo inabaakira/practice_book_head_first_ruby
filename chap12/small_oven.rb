b547594b3e584b521ebf9ad4392d752a8f74e060
INABA Akira <inaba@ulula.local>
2017-12-04 19:19:02 +0900

    [Chap.12] The end of the section: Updating our oven code with custom exception classes

--- chap12/small_oven.rb
+++ chap12/small_oven.rb
@@ -2,7 +2,13 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: small_oven.rb
 #    Created:       <2017/11/26 23:15:59>
-#    Last Modified: <2017/12/03 12:40:27>
+#    Last Modified: <2017/12/04 19:15:56>
+
+class OvenOffError < StandardError
+end
+
+class OvenEmptyError < StandardError
+end
 
 class SmallOven
   attr_accessor :contents
@@ -19,50 +25,27 @@ class SmallOven
 
   def bake
     unless @state == "on"
-      raise "You need to turn the oven on first!"
+      raise OvenOffError, "You need to turn the oven on first!"
     end
 
     if @contents == nil
-      raise "There's nothing in the oven!"
+      raise OvenEmptyError, "There's nothing in the oven!"
     end
 
     "golden-brown #{contents}"
   end
 end
 
-# dinner = ['turkey', 'casserole', 'pie']
-# oven = SmallOven.new
-# oven.turn_on
-# dinner.each do |item|
-#   begin
-#     oven.contents = item
-#     puts "Serving #{oven.bake}"
-#   rescue => error 
-#     puts "Error: #{error.message}"
-#   end
-# end
-
-# dinner = ['turkey', nil, 'pie']
-# oven = SmallOven.new
-# oven.turn_on
-# dinner.each do |item|
-#   begin
-#     oven.contents = item
-#     puts "Serving #{oven.bake}"
-#   rescue => error 
-#     puts "Error: #{error.message}"
-#   end
-# end
-
-dinner = ['turkey', 'casserole', 'pie']
+dinner = ['turkey', nil, 'pie']
 oven = SmallOven.new
 oven.turn_off
 dinner.each do |item|
   begin
     oven.contents = item
     puts "Serving #{oven.bake}"
-  rescue => error 
+  rescue OvenEmptyError => error
     puts "Error: #{error.message}"
+  rescue OvenOffError => error
+    oven.turn_on
   end
 end
-
