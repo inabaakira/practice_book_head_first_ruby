2c5a639dd0a6f24310e326bfcb2af3d6814a4c79
INABA Akira <inaba@ulula.local>
2017-12-12 20:25:17 +0900

    [Chap.13] The end of the section: Getting the test to pass

--- chap13/lib/list_with_commas.rb
+++ chap13/lib/list_with_commas.rb
@@ -2,21 +2,24 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: list_with_commas.rb
 #    Created:       <2017/12/08 20:18:45>
-#    Last Modified: <2017/12/12 19:55:04>
+#    Last Modified: <2017/12/12 20:24:06>
 
 class ListWithCommas
   attr_accessor :items
   def join
+    if items.length == 2
+      return "#{items[0]} and #{items[1]}"
+    end
     last_item = "and #{items.last}"
     other_items = items.slice(0, items.length - 1).join(', ')
     "#{other_items}, #{last_item}"
   end
 end
 
-# three_subjects = ListWithCommas.new
-# three_subjects.items = ['my parent', 'a rodeo clown', 'a prize bull']
-# puts "A photo of #{three_subjects.join}"
+two_subjects = ListWithCommas.new
+two_subjects.items = ['my parent', 'a rodeo clown']
+puts "A photo of #{two_subjects.join}"
 
-# two_subjects = ListWithCommas.new
-# two_subjects.items = ['my parent', 'a rodeo clown']
-# puts "A photo of #{two_subjects.join}"
+three_subjects = ListWithCommas.new
+three_subjects.items = ['my parent', 'a rodeo clown', 'a prize bull']
+puts "A photo of #{three_subjects.join}"
