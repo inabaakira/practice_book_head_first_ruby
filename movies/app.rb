c9d3743e0e8507ef40d434a21c86801d264cbf6f
INABA Akira <inaba@ulula.local>
2017-12-14 20:52:52 +0900

    [Chap.14] The end of the section: A route for the list of movies

---
+++ movies/app.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+#-*- mode: ruby; coding: utf-8 -*-
+# file: app.rb
+#    Created:       <2017/12/14 20:51:22>
+#    Last Modified: <2017/12/14 20:51:44>
+
+require 'sinatra'
+
+get('/movies') do
+  'Comming Soon...'
+end
