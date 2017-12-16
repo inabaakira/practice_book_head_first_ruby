fbb50b68e00dc9422039dd8a0676a3aee89414e4
INABA Akira <inaba@ulula.local>
2017-12-16 13:32:20 +0900

    [Chap.14] The end of the section: Setting up a Movie object in the Sinatra app

--- movies/app.rb
+++ movies/app.rb
@@ -2,10 +2,13 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: app.rb
 #    Created:       <2017/12/14 20:51:22>
-#    Last Modified: <2017/12/16 10:03:49>
+#    Last Modified: <2017/12/16 13:30:44>
 
 require 'sinatra'
+require 'movie'
 
 get('/movies') do
+  @movie = Movie.new
+  @movie.title = "Jaws"
   erb :index
 end
