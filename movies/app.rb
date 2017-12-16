8ba1383702ee23593149551aa369bc3905a0cf22
INABA Akira <inaba@ulula.local>
2017-12-16 10:11:42 +0900

    [Chap.14] The end of the section: Accessing the HTML from Sinatra

--- movies/app.rb
+++ movies/app.rb
@@ -2,10 +2,10 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: app.rb
 #    Created:       <2017/12/14 20:51:22>
-#    Last Modified: <2017/12/14 20:51:44>
+#    Last Modified: <2017/12/16 10:03:49>
 
 require 'sinatra'
 
 get('/movies') do
-  'Comming Soon...'
+  erb :index
 end
