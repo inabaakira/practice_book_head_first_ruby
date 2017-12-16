14687a69bf0bd298e3d6ac13bf6c4394c3ade94b
INABA Akira <inaba@ulula.local>
2017-12-16 12:36:48 +0900

    [Chap.14] The end of the section: A class to hold our movie data

---
+++ movies/lib/movie.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+#-*- mode: ruby; coding: utf-8 -*-
+# file: movie.rb
+#    Created:       <2017/12/16 12:32:56>
+#    Last Modified: <2017/12/16 12:33:21>
+
+class Movie
+  attr_accessor :title, :director, :year
+end
