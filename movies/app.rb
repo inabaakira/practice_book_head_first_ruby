8c59326213a6fbb1b8d0dcce587ab4d5f370c166
INABA Akira <inaba@ulula.local>
2017-12-17 10:01:46 +0900

    [Chap.14] The end of the section: Getting an HTML form for adding a movie

--- movies/app.rb
+++ movies/app.rb
@@ -2,7 +2,7 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: app.rb
 #    Created:       <2017/12/14 20:51:22>
-#    Last Modified: <2017/12/16 13:30:44>
+#    Last Modified: <2017/12/16 23:41:26>
 
 require 'sinatra'
 require 'movie'
@@ -11,4 +11,8 @@ get('/movies') do
   @movie = Movie.new
   @movie.title = "Jaws"
   erb :index
+end
+
+get ('/movies/new') do
+  erb :new
 end
